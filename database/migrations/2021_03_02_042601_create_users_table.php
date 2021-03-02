1376c3cb8df9ba35ddba768522c5d55f139681be
shaakilahammed <shaakilahammed@gmail.com>
2021-03-02 11:10:24 +0600

    M1F1

---
+++ database/migrations/2021_03_02_042601_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('user_name')->unique();
+            $table->string('email')->unique();
+            $table->string('type');
+            $table->string('password');
+            $table->string('city');
+            $table->string('country');
+            $table->string('phone');
+            $table->string('active')->default(1);
+            $table->string('company_name');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	<h1>Login Page</h1>
+
+	<form method="post">
+        @csrf
+
+		<fieldset>
+			<legend>Login</legend>
+			<table>
+				<tr>
+					<td>Email</td>
+					<td><input type="text" name="email"></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="password" name="password"></td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="submit" value="Submit"></td>
+					<td></td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+
+    {{-- {{session('msg')}} --}}
+
+</body>
+</html>
