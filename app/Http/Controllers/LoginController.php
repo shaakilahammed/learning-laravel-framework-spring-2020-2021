c0577adad18002574c1db932d669bb5531bb71c1
shaakilahammed <shaakilahammed@gmail.com>
2021-03-02 11:41:59 +0600

    M1F2

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use App\Http\Requests\LoginRequest;
 
 class LoginController extends Controller
 {
     public function index(){
         return view('login');
     }
-    public function verify(){
-        return "working";
+
+    public function verify(LoginRequest $req){
+        // $valid=Validator::make($req->all(),[
+        //     'email' => 'required|email|max:50',
+        //     'password' => 'required|min:8|max:20|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+        // ]);
+
+        // if($valid->fails()){
+        //     $request->session()->flash('error', 'Invalid username or password');
+        //     return redirect('/');
+        // }
+        return response()->json(['success'=>"true"],200);
     }
     
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,11 +14,19 @@
 			<table>
 				<tr>
 					<td>Email</td>
-					<td><input type="text" name="email"></td>
+					<td><input type="text" name="email" value="{{ old('email') }}">
+                        @error('email')
+                        <span style="margin:auto; color:red">{{ $message }}</span>
+                        @enderror
+                    </td>
 				</tr>
 				<tr>
 					<td>Password</td>
-					<td><input type="password" name="password"></td>
+					<td><input type="password" name="password" value="{{ old('password') }}">
+                        @error('password')
+                        <span style="margin:auto; color:red">{{ $message }}</span>
+                        @enderror
+                    </td>
 				</tr>
 				<tr>
 					<td><input type="submit" name="submit" value="Submit"></td>
